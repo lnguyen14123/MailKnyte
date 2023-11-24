842477e10302567855c0d941c4bf044882f34260
Luc Nguyen <lucnguyen1416@gmail.com>
2023-11-23 16:31:57 -0800

    Finish backend, create blog page, polish front end usability

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -4,43 +4,73 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.querySelector('form');
-  
+
     form.addEventListener('submit', function (event) {
-      event.preventDefault();
-  
-      const textareaContent = document.getElementById('demo-message').value;
-  
-      // Make a POST request to the backend
-      fetch('http://localhost:3000/api/scan-email', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          content: textareaContent,
-        }),
-      })
-        .then(response => response.json())
-        .then(data => {
-          console.log('Response from backend:', data);
-          // Handle the response, update UI, etc.
+        event.preventDefault();
 
-          const predictions = data.predictions;
+        const textareaContent = document.getElementById('demo-message').value;
 
-          // Update the UI based on the predictions
-          if (predictions.includes('Phishing Email')) {
-            // Display a warning or take appropriate action
-            alert('Warning: This email may be a phishing email!');
-          } else {
-            // Display a success message or other appropriate action
-            alert('Success: This email is not identified as phishing.');
-          }
+        emailContent = textareaContent.trim().replace(/\s+$/, '');
 
-        })
-        .catch(error => {
-          console.error('Error:', error);
-          // Handle errors gracefully
-        });
+        if (emailContent == "") {
+
+            alert('Warning: Invalid email contents!');
+
+
+        } else {
+            // Make a POST request to the backend
+            fetch('http://localhost:3000/api/scan-email', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    content: textareaContent,
+                }),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Response from backend:', data);
+                    // Handle the response, update UI, etc.
+
+                    const predictions = data.predictions;
+                    const probabilities = data.probabilities;
+
+                    var resultsSection = document.getElementById("first");
+                    resultsSection.scrollIntoView({ behavior: 'smooth' });
+
+                    var dangerRes = document.getElementById("danger-res");
+                    var safeRes = document.getElementById("safe-res");
+                    var conRes = document.getElementById("con-res");
+                    
+                    var percentDanger = (probabilities[0][0]) * 100;
+                    var percentSafe = (probabilities[0][1]) * 100;
+
+                    console.log(data)
+                    
+                    dangerRes.innerHTML = "MailBuddy says that this email has a " + percentDanger +" % chance of being potentially dangerous!";
+                    safeRes.innerHTML = "MailBuddy says that this email has a " + percentSafe + "% chance of being safe!";
+
+
+                    if (predictions.includes('Phishing Email')) {
+                        // Display a warning or take appropriate action
+                        conRes.innerHTML = "MailBuddy thinks this email is suspicious or dangerous. Be careful!";
+
+                    } else {
+                        // Display a success message or other appropriate action
+                        conRes.innerHTML = "MailBuddy thinks this email is safe. If you recognize the sender, its all ok!";
+
+                    }
+
+
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    // Handle errors gracefully
+                });
+
+        }
+
+
     });
-  });
-  
+});
